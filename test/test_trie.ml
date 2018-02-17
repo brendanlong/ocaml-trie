aa6d0cf5ceed3b68511b7b66b9532d670b9ba3f0
Brendan Long <self@brendanlong.com>
2018-02-17 11:09:49 -0500

    Use QCheck instead of OUnit

--- test/test_trie.ml
+++ test/test_trie.ml
@@ -1,5 +1,3 @@
-open OUnit2
-
 module CharMap = Map.Make(struct
     type t = char
     let compare = Char.compare
@@ -7,18 +5,41 @@ module CharMap = Map.Make(struct
 
 module CharTrie = Trie.Make(CharMap)
 
+let suite =
+  let open QCheck in
+  [ Test.make ~name:"empty trie is empty"
+      ~count:1 unit (fun () -> CharTrie.(is_empty empty))
+  ; Test.make ~name: "empty trie mem always false"
+      (list char) (fun key -> not CharTrie.(mem key empty))
+  ; Test.make ~name:"empty trie find always raises"
+    (list char) (fun key ->
+    try
+      ignore CharTrie.(find key empty);
+      false
+    with Not_found -> true)
+  ; Test.make ~name:"empty trie remove returns empty trie"
+    (list char) (fun key ->
+    CharTrie.(remove key empty |> equal (=) empty))
+  ; Test.make ~name:"same trie is equal"
+    (list (pair (small_list char) int)) (fun kv_list ->
+    let make_trie () =
+      List.fold_left (fun acc (key, value) ->
+        CharTrie.add key value acc)
+        CharTrie.empty kv_list
+    in
+    let trie1 = make_trie () in
+    let trie2 = make_trie () in
+    CharTrie.equal (=) trie1 trie2)
+  ; Test.make ~name:"same trie compares equal"
+    (list (pair (small_list char) string)) (fun kv_list ->
+    let make_trie () =
+      List.fold_left (fun acc (key, value) ->
+        CharTrie.add key value acc)
+        CharTrie.empty kv_list
+    in
+    let trie1 = make_trie () in
+    let trie2 = make_trie () in
+    CharTrie.compare String.compare trie1 trie2 = 0) ]
+
 let () =
-  [ "empty" >:: fun _ ->
-      let trie = CharTrie.empty in
-      CharTrie.is_empty trie
-      |> assert_bool "empty trie is empty";
-      assert_raises ~msg:"empty trie can't find 'a'" Not_found (fun () ->
-        CharTrie.find ['a'] trie);
-      assert_bool "empty trie can't mem 'a'"
-        (not (CharTrie.mem ['a'] trie));
-      assert_bool "empty trie equals empty trie"
-        (CharTrie.equal Char.equal trie trie);
-      assert_equal ~msg:"empty trie compares equals with empty trie" 0
-        (CharTrie.compare Char.compare trie trie) ]
-  |> test_list
-  |> run_test_tt_main
+  QCheck_runner.run_tests_main suite
